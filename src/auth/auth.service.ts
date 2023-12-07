c0b5b374de10994fec195236311095f1ed04a838
이상윤 <zaar625@gmail.com>
2023-12-07 12:47:13 +0900

    add : Token Refresh 기능 정리하기

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -12,6 +12,24 @@ export class AuthService {
     private readonly usersService: UsersService,
   ) {}
   /**
+   * 토큰을 사용하게 되는 방식
+   * 1) 사용자가 로그인 또는 회원가입을 진행하면 accessToken과 refreshToken을 발급받는다.
+   * 2) 로그인 할때는 Basic 토근과 함께 요청을 보낸다. Basic 토큰은 '이메일:비밀번호'를 Base64로 인코딩한 형태이다.
+   * 예){authorization:'Basic {token}'}
+   * 3) 아무나 접근 할 수 없는 정보 (private route)를 접근 할때는 accessToken을 Header에 추가해서 요청과 함께 보낸다.
+   * 예) {authorization :'Bearer:{token}'}
+   * 4) 토큰과 요청을 함께 받은 서버는 토큰 검증을 통해 현재 요청을 보낸 사용자가 누구인지 알 수 있다.
+   * 예를 들어서 현재 로그인한 사용자가 작성한 포스트만 가져오려면 토큰의 sub 값에 입력되있는 사용자의 포스트만 따로
+   * 필터링 할 수 있다. 특정 사용자의 토큰이 없다면 다른 사용자의 데이터를 접근 못한다.
+   * 5)모든 토큰은 만료 기간이 있다. 만료기간이 지나면 새로 토큰을 발급받아야한다.
+   * 그렇지 않으면 jwtService.verify()에서 인증이 통과 안된다. 그러니 access 토큰을 새로 발급 받을 수 있는 /auth/token/access와 refresh
+   * 토큰을 새로 발급 받을 수 있는 /auth/token/refresh가 필요하다.
+   * 6)토큰이 만료되면 각각의 토큰을 새로 발급 받을 수 있는 엔드포인에 요청을 해서 새로운 토큰을 발급받고 새로운
+   * 토큰을 사용해서 private route에 접근한다.
+   *
+   */
+
+  /**
    * 우리가 만들려는 기능
    * 1. registerWithEmail
    *  - email, nickname, password를 입력받고 사용자를 생성한다.
